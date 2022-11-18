d338a23653be5eb1343ec32ff37215ce44220d7c
test <test@test.com>
2022-11-18 14:28:53 +0700

    Add unit test for wager_buy.go

--- service/wager/logic/wager_list_test.go
+++ service/wager/logic/wager_list_test.go
@@ -1,1 +1,46 @@
 package wagerlogic
+
+import (
+	"github.com/stretchr/testify/assert"
+	"rk_echo/db/models"
+	"rk_echo/db/sqldb"
+	"rk_echo/pkg/dbutils"
+	"testing"
+)
+
+func TestList(t *testing.T) {
+
+	// Init DB
+	sqldb.InitFakeDB()
+
+	// Get list when db is empty, should return an empty list
+
+	res, err := List(dbutils.PaginateInput{})
+	if err != nil {
+		t.Fatal("this should not fail")
+	}
+	assert.Equal(t, 0, len(*res))
+
+	// Init some data in the db
+
+	for i := 0; i < 100; i++ {
+		sqldb.DB.Create(&models.Wager{})
+	}
+
+	res, err = List(dbutils.PaginateInput{})
+	if err != nil {
+		t.Fatal("this should not fail")
+	}
+	assert.Equal(t, 100, len(*res))
+
+	// Check pagination logic
+	res, err = List(dbutils.PaginateInput{
+		Page: 3,
+		Size: 10,
+	})
+	assert.Equal(t, 10, len(*res))
+
+	for i := 0; i < 10; i++ {
+		assert.True(t, (*res)[i].Id > 29)
+	}
+}
--- service/wager/logic/wager_place_test.go
+++ service/wager/logic/wager_place_test.go
@@ -1,1 +1,39 @@
 package wagerlogic
+
+import (
+	"github.com/stretchr/testify/assert"
+	"rk_echo/db/sqldb"
+	wagerdto "rk_echo/service/wager/dto"
+	"testing"
+)
+
+func TestPlace(t *testing.T) {
+
+	// Init DB
+	sqldb.InitFakeDB()
+
+	// Init a wager with selling price < total wager value * selling percentage
+	input := &wagerdto.PlaceWagerInput{
+		TotalWagerValue:   200,
+		Odds:              100,
+		SellingPercentage: 100,
+		SellingPrice:      100,
+	}
+
+	_, err := Place(input)
+	if err == nil {
+		t.Fatal("this should fail")
+	}
+
+	// Changing to a valid wager
+	input.SellingPrice = 300
+
+	wager, err := Place(input)
+	if err != nil {
+		t.Fatal("this should not fail")
+	}
+
+	assert.Equal(t, 300, wager.SellingPrice)
+	assert.Equal(t, 0, wager.AmountSold)
+	assert.Equal(t, 0, wager.PercentageSold)
+}
